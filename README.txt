2010c1f9ad1a536fe71e37b7c82891d28fe7f186
keithf1991 <blahBLAH1991>
2013-06-24 21:41:48 -0400

    Adding updated files to the repo

---
+++ README.txt
@@ -0,0 +1,7 @@
+main.java
+	Uses import_movies.txt to updates red dwarf database. 
+	This version should run without arguments.
+
+db_helper.java
+	Basic CRUD functionality. Most usage is documented within the program itself.
+	May need to be run with java -cp .:postgres.jar <file location> db_helper
---
+++ db_helper.java
@@ -0,0 +1,470 @@
+import java.io.*;
+import java.sql.*;
+import java.util.Enumeration;
+import java.util.Vector;
+import java.util.Iterator;
+
+
+public class db_helper {
+	
+	Connection server;
+	Statement stmt;
+	
+	/*
+	 * db_helper constructor
+	 */
+	public db_helper(){
+	    //connecting to red dwarf
+	    try {
+	    	//this is temporary.  just for debugging purposes.
+		    String red_dwarf_url = "jdbc:postgresql://reddwarf.cs.rit.edu/p48501b"; 
+			server = DriverManager.getConnection(red_dwarf_url, "p48501b", "heifohhitheihiqu");
+			stmt = server.createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("connection failed");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	/*
+	 * dumps all movie data into string except for id
+	 * @return movie_names
+	 */
+	public Vector<String> getMoviedata(String searchTitle){
+		Vector<String> movie_names = new Vector<String>();
+		System.out.println("querying " + searchTitle + " in movies" );
+		
+		String query = "select * from movie where title like '%" + searchTitle + "%';";
+		
+		//S/ystem.out.println("created query for SQL");
+		try {
+			//System.out.println("query: " + query);
+			
+			ResultSet movies = stmt.executeQuery(query);
+			while(movies.next()){
+				String i = movies.getString("mid");
+				String t = movies.getString("title");
+				String g = movies.getString("genre");
+				String ru = movies.getString("runtime");
+				String ra = movies.getString("rating");
+				String p = movies.getString("plot");
+				String y = movies.getString("production_year");
+				String entry = t + " \t " + g + " \t " + ru + " \t " + ra + " \t " + y + "\nPlot:  " + p;
+				movie_names.add(entry);
+			}
+		}catch(SQLException e){
+			System.err.println("error getting movie names\n" + e);
+		}catch(NullPointerException d){
+			System.err.println("null pointer exception" + d);
+		}
+		
+		return movie_names;
+	}
+	
+	
+	
+	/*
+	 * get movie names when given a string query
+	 * @return movie_names
+	 */
+	public Vector<String> getMovieNamesbyTitle(String searchTitle){
+		Vector<String> movie_names = new Vector<String>();
+		System.out.println("querying " + searchTitle + " in movies");
+		
+		String query = "select title from movie where title like '%" + searchTitle + "%';";
+		
+		//S/ystem.out.println("created query for SQL");
+		try {
+			//System.out.println("query: " + query);
+			
+			ResultSet movies = stmt.executeQuery(query);
+			System.out.println("got movie names");
+			while(movies.next()){
+				String t = movies.getString("title");
+				movie_names.add(t);
+			}
+		}catch(SQLException e){
+			System.err.println("error getting movie names\n" + e);
+		}catch(NullPointerException d){
+			System.err.println("null pointer exception" + d);
+		}
+		
+		return movie_names;
+	}
+	
+	/*
+	 * get movie names when given a string query
+	 * @return movie_names
+	 */
+	public Vector<String> getPeopleNamesbyNames(String name){
+		Vector<String> people_names = new Vector<String>();
+		System.out.println("querying " + name + " in people");
+		
+		String query = "select name from person where name like '%" + name + "%';";
+		
+		try {
+		
+			
+			ResultSet people = stmt.executeQuery(query);
+			while(people.next()){
+				String t = people.getString("name");
+				people_names.add(t);
+			}
+		}catch(SQLException e){
+			System.err.println("error getting movie names\n" + e);
+		}catch(NullPointerException d){
+			System.err.println("null pointer exception" + d);
+		}
+		return people_names;
+	}
+	
+	/*
+	 * get people names when given a string query
+	 * @return movie_names
+	 */
+	public Vector<String> getPeopleData(String name){
+		Vector<String> people_names = new Vector<String>();
+		System.out.println("querying " + name + " in people" );
+		
+		String query = "select name,gender from person where name like '%" + name + "%';";
+		
+		try {
+		
+			
+			ResultSet people = stmt.executeQuery(query);
+			System.out.println("got people names");
+			while(people.next()){
+				String t = people.getString("name");
+				String g = people.getString("gender");
+				String name_string = t + "\t" + g;
+				people_names.add(name_string);
+			}
+		}catch(SQLException e){
+			System.err.println("error getting people names\n" + e);
+		}catch(NullPointerException d){
+			System.err.println("null pointer exception" + d);
+		}
+		return people_names;
+	}
+	
+	/*
+	 * get people names when given a string query
+	 * @return movie_names
+	 */
+	public Vector<String> getCharacterData(String name){
+		Vector<String> char_names = new Vector<String>();
+		System.out.println("querying " + name + " in characters ");
+		
+		String query = "select name from characters where name like '%" + name + "%';";
+		
+		try {
+		
+			
+			ResultSet people = stmt.executeQuery(query);
+			System.out.println("got people names");
+			while(people.next()){
+				String t = people.getString("name");
+				char_names.add(t);
+			}
+		}catch(SQLException e){
+			System.err.println("error getting character names\n" + e);
+		}catch(NullPointerException d){
+			System.err.println("null pointer exception" + d);
+		}
+		return char_names;
+	}
+
+	//lets create a table
+	// variables(name, type, not null)
+	//primary_key(name)
+	//foreign key(attr referenced_table ref att);
+	public int createTable(String table_name, Vector<String> variables, Vector<String> primary_keys, Vector<String> foreign_keys){
+	
+		//extract variable data
+		String variablesString = "";
+		String primaryString = "";
+		String tmpString = "";
+		
+		System.out.println("table name is " + table_name);
+		//check data values 
+		for(int k = 0; k< variables.size()-1; k++){
+			
+			//System.out.println("print variables " + variables.elementAt(k));
+			variablesString = variablesString + variables.elementAt(k)+", ";
+		}
+		
+		for(int k = 0; k< primary_keys.size()-1; k++){
+			
+			//System.out.println("print primary " + primary_keys.elementAt(k));
+			primaryString += primary_keys.elementAt(k);
+		}
+		
+		
+		System.out.println("Variable String: " + variablesString);
+		System.out.println("Primary Keys: " + primaryString);
+		
+		/*
+		for(int k = 0; k< foreign_keys.size()-1; k++){
+			
+			System.out.println("print variables " + foreign_keys.elementAt(k));
+			
+		}*/
+		//extract primary keys
+		//extract foreign_keys	
+		//String query = "create table " + table_name"( );";
+		
+		
+		String query = "create table " + table_name + " (" + variablesString + " primary key(" + primaryString + "));";
+		
+		System.out.println("Create table query: " + query);
+		try{	
+			stmt.executeUpdate(query); 
+		}catch(SQLException e){
+			System.err.println("ERR: couldn't create table");
+			return 0;
+		}
+		
+		
+		return 1;
+	}
+
+	/*
+	 * deletes a table
+	 */
+	public int deleteTable(String table){
+		String query = "drop table " + table;
+	
+		/*try{
+			
+			//we'll add this later.  we don't want deletion yet. 
+			//stmt.executeUpdate(query); 
+		}catch(SQLException e){
+			System.err.println("ERR: couldn't delete table " + table);
+			return 0;
+		}*/
+		return 1;
+	}	
+	
+	
+	public int truncateTable(String table){
+		String query = "truncate table " + table;
+		/*try{
+		
+		//we'll add this later.  we don't want truncation yet. 
+		//stmt.executeUpdate(query); 
+	}catch(SQLException e){
+		System.err.println("ERR: couldn't truncate table " + table);
+		return 0;
+	}*/
+		return 1;
+	}
+	
+	
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		db_helper helper = new db_helper();
+	    System.out.println("connection success!");
+//		System.out.println("give me a query!");
+
+		String userInput = "";
+
+		
+		InputStreamReader inReader = new InputStreamReader(System.in);
+		BufferedReader bReader = new BufferedReader( inReader );
+								
+	    while (!userInput.equals("Q") && !userInput.equals("q"))
+		{
+		
+		System.out.println("IMDB database manager\n" + "C - Create\n" + "R - Retrieve\n" + 
+			"U - Update\n" + "D - Delete\n" + "Q - Quit\n" + "Select option:");
+
+
+				try{
+					userInput = bReader.readLine();		//read in user input
+				}catch(IOException e){
+					System.err.println("trouble reading query from console\n" + e);
+				}catch(NullPointerException d){
+					System.err.println(d.getStackTrace());
+				}
+			
+
+				if (userInput.equals("R") || userInput.equals("r"))
+				{
+
+						System.out.println("Enter search term:\n");
+
+						String query = "";
+						
+						try{
+							query = bReader.readLine();		//read in movie search
+						}catch(IOException e){
+							System.err.println("trouble reading query from console\n" + e);
+						}catch(NullPointerException d){
+							System.err.println(d.getStackTrace());
+						}
+	
+						Vector<String> movie_query_results;
+					
+						//get movie results on query
+						movie_query_results = helper.getMoviedata(query);	    
+						if(movie_query_results != null){
+							
+							Enumeration<String> listy = movie_query_results.elements();
+							
+							//inserting data into the red dwarf db
+							System.out.println("movie results:");
+
+							while(listy.hasMoreElements()){
+								//System.out.println("Inserting movie id: " + listy.nextElement());
+								String movieString = listy.nextElement();
+								System.out.println(movieString);
+							}	    
+						}
+						//get people results on query
+
+						Vector<String> people_query_results= helper.getPeopleData(query);	    
+						if(people_query_results != null){
+							
+							Enumeration<String> listy = people_query_results.elements();
+							
+							//inserting data into the red dwarf db
+							System.out.println("people results:");
+
+							while(listy.hasMoreElements()){
+								String peopleString = listy.nextElement();
+								System.out.println(peopleString);
+							}	    
+						}    
+						
+						//get character results on query
+						
+						Vector<String> char_query_results= helper.getCharacterData(query);	    
+						if(char_query_results != null){
+							
+							Enumeration<String> listy = char_query_results.elements();
+							
+							//inserting data into the red dwarf db
+							System.out.println("character results:");
+
+							while(listy.hasMoreElements()){
+								String peopleString = listy.nextElement();
+								System.out.println(peopleString);
+							}	    
+						}
+				}	
+				if (userInput.equals("c") || userInput.equals("C"))
+				{
+					String table_name_query = "";
+					Vector<String> variables = new Vector<String>();
+					Vector<String> primary_keys = new Vector<String>(); 
+					Vector<String> foreign_keys = new Vector<String>();;
+					
+					System.out.print("table name:");		
+
+					String query = "";
+						
+						try{
+							query = bReader.readLine();		//read in table name 
+							table_name_query = query;
+						}catch(IOException e){
+							System.err.println("trouble reading query from console\n" + e);
+						}catch(NullPointerException d){
+							System.err.println(d.getStackTrace());
+						}
+					
+					System.out.println("input variable data in this format (name type not null)");
+					System.out.println("name - any format\n type - sql data types\n not mull - write out not null");
+					while(!query.equals("")){
+						System.out.print("variable:");
+						try{
+							query = bReader.readLine();
+						}catch(IOException e){
+							System.err.println("trouble reading query from console\n" + e);
+						}catch(NullPointerException d){
+
+							System.err.println(d.getStackTrace());
+						}
+							//System.out.println("variable entered: " + query);
+							variables.add(query);
+							//System.out.println("after");
+					}
+					query = "1";
+					System.out.println("define a primary keys in format [attr name]");
+					while(!query.equals("")){
+						System.out.print("primary_key:");
+						try{
+							query = bReader.readLine();
+						}catch(IOException e){
+							System.err.println("trouble reading query from console\n" + e);
+						}catch(NullPointerException d){
+							System.err.println(d.getStackTrace());
+						}
+							primary_keys.add(query);
+					}
+
+					query = "1";
+					
+					/*
+					System.out.println("define a foreign key [attr name, table reference, reference attr]");
+					while(!query.equals("")){
+						System.out.print("foreign_key:");
+						try{
+							query = bReader.readLine();
+						}catch(IOException e){
+							System.err.println("trouble reading query from console\n" + e);
+						}catch(NullPointerException d){
+							System.err.println(d.getStackTrace());
+						}
+							foreign_keys.add(query);
+					}*/
+					
+					int x = helper.createTable(table_name_query, variables, primary_keys, foreign_keys);
+					
+					if(x == 1)
+						System.out.println("created table successfully");
+					else
+						System.out.println("error creating table");
+
+				}
+
+				if (userInput.equals("u") || userInput.equals("U"))
+				{
+					
+				}
+
+
+				if (userInput.equals("d") || userInput.equals("D"))
+				{
+					System.out.println("Which table do you want to delete?\n");
+
+						String qq= "";
+						
+						try{
+							qq= bReader.readLine();		//read in  table name
+						}catch(IOException e){
+							System.err.println("trouble reading query from console\n" + e);
+						}catch(NullPointerException d){
+							System.err.println(d.getStackTrace());
+						}
+
+
+					helper.deleteTable(qq);
+					System.out.println("THIS WILL DELETE A TABLE BUT WE DONT WANT THAT TO FUNCTION YET");
+				}
+
+
+			
+	    }
+   
+	   System.out.println("end of program"); 
+	}
+}
